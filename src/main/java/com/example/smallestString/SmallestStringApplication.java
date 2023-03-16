67fa678dbd22beb1aaacdf73b0ea7d83aa72e571
Liege Kluppel <liege.kluppel@banfico.com>
2023-03-16 17:49:13 -0300

    first commit

---
+++ src/main/java/com/example/smallestString/SmallestStringApplication.java
@@ -0,0 +1,13 @@
+package com.example.smallestString;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallestStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallestStringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smallestString/SmallestStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smallestString;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallestStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
